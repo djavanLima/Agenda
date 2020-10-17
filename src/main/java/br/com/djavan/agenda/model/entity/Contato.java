ad58031f7919d9196b56b27b179f5ad5d3dd3450
Djavan Lima <djavanlyonel@gmail.com>
2020-10-17 10:57:49 -0400

    inicial commit

---
+++ src/main/java/br/com/djavan/agenda/model/entity/Contato.java
@@ -0,0 +1,40 @@
+package br.com.djavan.agenda.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contato {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String nome;
+	
+	
+	@Column
+	private String email;
+	
+	
+	@Column
+	private Boolean favorito;
+	
+	
+	
+	
+}
